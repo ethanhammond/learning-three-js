b22b64fb45aa80a9770510efcadef42b51e8783e
Ethan Hammond <Ethan Hammond>
2016-10-04 16:00:54 -0400

    Created animation for sphere and cube

--- first-scene/scripts/main.js
+++ first-scene/scripts/main.js
@@ -2,27 +2,35 @@
  * Ethan Hammond
  * Date
  * Description
- * TODO:
+ * TODO: Add animation speed controls
  */
 
 "use strict";
+function init() {
 
-function createScene() {
+    var stats = initStats();
+
+    //Create a new scene
 	var scene = new THREE.Scene();
 
+    //Set camera position
 	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+    //Create renderer and set size
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setClearColorHex(0xaaaaaa);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.shadowMapEnabled = true;
 
+    //Add coordinate axis
 	var axes = new THREE.AxisHelper( 20 );
 	scene.add(axes);
 
+    //Set ground plane size and color
 	var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
 	var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
 
+    //Position the plane and add it to the scene
 	var plane = new THREE.Mesh(planeGeometry,planeMaterial);
 	 plane.rotation.x=-0.5*Math.PI;
 	 plane.position.x = 15;
@@ -31,69 +39,88 @@ function createScene() {
 	 plane.receiveShadow = true;
 	 scene.add(plane);
 
+    //Create cube
 	 var cubeGeometry = new THREE.CubeGeometry(4,4,4);
 	 var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
 	 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
+    //Position the cube
 	 cube.position.x = -3;
 	 cube.position.y = 3;
 	 cube.position.z = 0;
 
+    //Set cube to cast shadow and add to scene
 	 cube.castShadow = true;
-
 	 scene.add(cube);
 
+    //Create sphere
 	 var sphereGeometry = new THREE.SphereGeometry(4,20,20);
 	 var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
 	 var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
 
+     //Position the sphere
 	 sphere.position.x = 20;
 	 sphere.position.y = 4;
 	 sphere.position.z = 2;
 
+    //Set sphere to cast shadow and add to scene
 	 sphere.castShadow = true;
-
 	 scene.add(sphere);
 
+    //Set camera position and orientation
 	 camera.position.x = -30;
 	 camera.position.y = 40;
 	 camera.position.z = 30;
 	 camera.lookAt(scene.position);
 
+    //Create spotlight aimed at objects and add to scene
 	 var spotLight = new THREE.SpotLight( 0xffffff);
 	 spotLight.position.set( -40, 60, -10);
 	 scene.add(spotLight);
 
+    //Allow shadows to be rendered by the spotlight
 	 spotLight.castShadow = true;
 
+    //Place output of renderer in HTML
 	 $("#WebGL-output").append(renderer.domElement);
-	 renderer.render(scene, camera);
-}
 
-function initStats() {
-	var stats = new Stats();
-	stats.setMode(0);
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.left = '0px';
-	stats.domElement.style.top = '0px';
-	$("#Stats-output").append( stats.domElement );
-	return stats;
-}
+    //Prep for animations
+     var step = 0;
+	 renderScene();
 
-function renderScene() {
-	stats.update();
+    function renderScene() {
 
-	cube.rotation.x += 0.02;
-    cube.rotation.y += 0.02;
-    cube.rotation.z += 0.02;
+        //Update FPS or render-time counter
+        stats.update();
 
-	requestAnimationFrame(renderScene);
-	renderer.render(scene, camera);
+        //Rotate the cube
+        cube.rotation.x += 0.02;
+        cube.rotation.y += 0.02;
+        cube.rotation.z += 0.02;
+
+        //Bounce the sphere
+        step+=0.04;
+        sphere.position.x = 20+( 10*(Math.cos(step)));
+        sphere.position.y = 2 +( 10*Math.abs(Math.sin(step)));
+
+        //Render the animation
+        requestAnimationFrame(renderScene);
+        renderer.render(scene, camera);
+    }
+
+    function initStats() {
+
+        var stats = new Stats();
+        stats.setMode(0);
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        $("#Stats-output").append( stats.domElement );
+        return stats;
+    }
+
 }
 
 window.addEventListener("load", ()=>{
-    console.log("Hello World!");
-    createScene();
-    initStats();
-    renderScene();
+    init();
 });
